c755954d041f8e8c2da3a86e5df758861ea28757
JiaXingVV <151317780+JiaXingVV@users.noreply.github.com>
2023-11-22 16:32:59 +0800

    Update App.js

--- crm/src/App.js
+++ crm/src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Route path="Account" element={<Account />} />
           <Route path="Contact" element={<Contact />} />
           <Route path="Opportunity" element={<Opportunity />} />
+          <Route path="quotation" element={<quotation />} />
         </Route>
       </Routes>
     </BrowserRouter>
